e3da1aaf4b98d018824b89d015b991408a09b5e5
TK <tiankaixie@gmail.com>
2023-06-02 14:26:07 -0700

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <>
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
@@ -41,14 +40,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
           )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
+            <div className="relative flex min-h-screen flex-col">
               <div className="flex-1">{children}</div>
             </div>
             <TailwindIndicator />
           </ThemeProvider>
         </body>
       </html>
-    </>
   )
 }
