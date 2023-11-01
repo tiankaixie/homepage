751652cac0ace282eb8746fe09cdebdcf40736b3
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-11-01 08:48:28 -0700

    adjust the theme toggle button

--- components/site-sider.tsx
+++ components/site-sider.tsx
@@ -8,17 +8,19 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteSider() {
   return (
-    <div className="fixed flex flex-col w-56 h-[calc(100vh-1rem)]  bg-primary  top-0 m-2 p-2  ">
+    <div className="fixed top-0 m-2 flex h-[calc(100vh-1rem)] w-56 flex-col  justify-between bg-primary p-2  ">
       <div className="">
         <Link href="/" className="flex items-center space-x-2">
           {/* <Icons.logo className="h-6 w-6" /> */}
-          <span className="inline-block font-serif italic text-2xl text-primary-foreground mb-5 ml-3">
+          <span className="mb-5 ml-3 inline-block font-serif text-2xl italic text-primary-foreground">
             {siteConfig.name}
           </span>
         </Link>
         <MainNav items={siteConfig.mainNav} />
-        <div className="text-xs font-semibold mt-4 mb-2 ml-2">Contact</div>
+        <div className="mb-2 ml-2 mt-4 text-xs font-semibold">Contact</div>
         <MainNav items={siteConfig.socialNav} />
+      </div>
+      <div>
         <ThemeToggle />
       </div>
     </div>
